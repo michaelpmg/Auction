82988262eff34f953a5616485d23629691d62bf7
Michael Perreault <michaelp@MiraGeoscience.local>
2025-03-26 18:49:48 -0400

    fixed ps1 script python install

--- installer_shell_script.ps1
+++ installer_shell_script.ps1
@@ -11,16 +11,14 @@ $DIR=$PSScriptRoot
 
 $python_install_job = Start-Job { .\python-3.9.0.exe InstallAllUsers=0 PrependPath=1 Include_test=0 }
 
-if (-Not (Get-Command 'py' -errorAction SilentlyContinue)){
-	echo "--- Installation de python 3.9 ---"
-	echo "Telechargement de l'installeur python."
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-	Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe" -OutFile ".\python-3.9.0.exe"
-	echo "Installing Python, please rerun the installer once completed by running .\installer.ps1"
+echo "--- Installation de python 3.9 ---"
+echo "Telechargement de l'installeur python."
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe" -OutFile ".\python-3.9.0.exe"
+echo "Installing Python, please rerun the installer once completed by running .\installer.ps1"
 
-	Wait-Job $python_install_job
-	Receive-Job $python_install_job
-}
+Wait-Job $python_install_job
+Receive-Job $python_install_job
 
 rm .\python-3.9.0.exe -Force 2>$null
 echo "Installation des modules de dependances."
