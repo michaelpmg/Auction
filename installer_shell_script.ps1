fdf86bd74eac33bad6951524ed24cb72860d7c84
Michael Perreault <michaelp@MiraGeoscience.local>
2025-03-26 18:40:08 -0400

    fixed ps1 script

--- installer_shell_script.ps1
+++ installer_shell_script.ps1
@@ -7,14 +7,12 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 echo "Installation du generateur de facture d'encan"
-Pause
 $DIR=$PSScriptRoot
 
 $python_install_job = Start-Job { .\python-3.9.0.exe InstallAllUsers=0 PrependPath=1 Include_test=0 }
 
 if (-Not (Get-Command 'py' -errorAction SilentlyContinue)){
 	echo "--- Installation de python 3.9 ---"
-	Pause
 	echo "Telechargement de l'installeur python."
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 	Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe" -OutFile ".\python-3.9.0.exe"
@@ -22,15 +20,12 @@ if (-Not (Get-Command 'py' -errorAction SilentlyContinue)){
 
 	Wait-Job $python_install_job
 	Receive-Job $python_install_job
-	Pause
 }
 
 rm .\python-3.9.0.exe -Force 2>$null
 echo "Installation des modules de dependances."
-Pause
 py -m pip install -r requirements.txt
 Read-Host "Installation complete appuyez sur [Entrer] pour continuer."
-Pause
 
 
 	
